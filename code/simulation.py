658db9c2db7b9b2e8f0ea043c5e8934e40b13984
Matteo Piraccini <matteo.piraccini@outlook.it>
2023-02-22 13:09:08 +0100

    22/02/2023 13:09

--- code/simulation.py
+++ code/simulation.py
@@ -1,1 +1,21 @@
-# qui ci va la simulazione
+import numpy as np
+
+def init_simulation()# che parametri ci vanno?
+	"""
+    Create the 0-generation and initialize the structures that contains data
+    Parameters:
+    -----------------------------
+
+    -----------------------------
+    Returns the area between the two fronts
+    """
+	#bisogna creare la popolazione e tutti gli oggetti per contenere le informazioni
+	population = np.zeros(2,100) #creo un'array 2x100, che rappresenta la popolazione, nella prima riga metto la strategia k,nella seconda le risorse
+	# ora bisogna creare la strategia per ogni individuo
+	for individual in population[0]
+		individual = 
+
+def generate_random_int(low_value, high_value, sample)# tengo la generazione randomica separate per il testing
+	np.random.randint(low_value, high_value, sample, dtype=np.short)
+		
+	
