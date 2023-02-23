8f33630172358fd5290da41e1e70d53d1c2285bc
Matteo Piraccini <matteo.piraccini@outlook.it>
2023-02-23 13:40:49 +0100

    23/02/2023 13:23

--- code/simulation.py
+++ code/simulation.py
@@ -10,12 +10,14 @@ def init_simulation():# che parametri ci vanno?
     	Returns
     	"""
 	#bisogna creare la popolazione e tutti gli oggetti per contenere le informazioni
-	population = np.zeros(2,100) #creo un'array 2x100, che rappresenta la popolazione, nella prima riga metto la strategia k,nella seconda le risorse
-	# ora bisogna creare la strategia per ogni individuo
-	for individual in population[0]
-		individual = 
+	list=[]
+	population=np.zeros((2,100))
+	for x in range(100):
+		list.append(generate_random_int(-5, +6))
+	population[0]=lista
+		
 
-def generate_random_int(low_value, high_value, sample):# tengo la generazione randomica separate per il testing
-	np.random.randint(low_value, high_value, sample, dtype=np.short)
+def generate_random_int(low_value, high_value):# tengo la generazione randomica separate per il testing
+	np.random.randint(low_value, high_value, None, dtype=np.short)
 		
 	
