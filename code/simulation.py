e51d3459e8d15245cfd31a44cd43ee1fb4b698bb
Matteo Piraccini <matteo.piraccini@outlook.it>
2023-02-23 22:42:14 +0100

    23/02/2023 22:42

--- code/simulation.py
+++ code/simulation.py
@@ -1,33 +1,67 @@
 import numpy as np
 
+from collections import namedtuple
+
+Data = namedtuple( 'Data', 
+	[ 'population' , 
+	'sources',
+	'count_type'])
+
 def init_simulation(): # che parametri ci vanno?
+
 	"""
+
     	Create the 0-generation and initialize the structures that contains data
+
     	Parameters:
+
     	-----------------------------
 	
    	-----------------------------
+
     	Returns
 	
     	"""
+
 	#creation of the 0-generation
-	population=np.zeros((2,100))
-	population[0]=np.array(generate_random_int(-5, +7, (1,100), np.short))
+
+	population=np.zeros(100, np.byte)
+	
+	population[0] = np.array(generate_random_int(-5, +7, (1,100), np.short)) #7 is excluse from the random number
+
+	#setup of a separate array for the source of every individual, in order to save memory
+	
+	sources = np.zeros(100, np.half)
+
 	#setup of the matrix of reputation
-	reputation=np.zeros((100,100), np.byte)
+
+	reputation = np.zeros((100,100), np.byte)
+
 	#setup the matrix that will count the amount of every type related to generation
-	population_type=np.zeros((12, 1))
-	return (population,reputation)
+
+	pop_count_type = np.array(count_population(population[0]))
+
+	O_data=Data(population,reputation, pop_count_type)
+
+	return O_data
+
+
+############################################# controllare se tupla permette di memorizzare diversi tipi di array
 		
 
 def generate_random_int(low_value, high_value, size, dtype):# tengo la generazione randomica separate per il testing
+
 	return np.random.randint(low_value, high_value, size, dtype)
 
 def count_population(array_pop):
+
+	count_row=np.zeros(12,np.byte)
+
 	for x in range(-5,+7):
+
 		count_row[x]=len(array_pop[array_pop==x])
 
-	print(count_row)
+	return count_row
 
 	
 		
--- code/testing.py
+++ code/testing.py
@@ -3,6 +3,7 @@ import numpy as np
 import simulation as sm
 
 def testing_init():
+	# just a visual check
 	zero=np.zeros((2,100))
 	a=sm.init_simulation()[0]
 	print(a)
@@ -10,6 +11,10 @@ def testing_init():
 	assert a==zero
 
 def testing_count(min,max):
-	a=np.array(np.random.randint(min, max, 100, byte))
-	sm.count_population(a)
-	# finire di creare il test
+	# check if the counter work
+	a=np.array(np.random.randint(min, max, 100, np.byte))
+	count_row=sm.count_population(a)
+	print(count_row)
+	print(a)
+	if(min==-5 & max==7):
+		assert np.sum(count_row)==100
