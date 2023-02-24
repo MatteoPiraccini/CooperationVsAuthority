3123fd0cec485c31192b54e8c8cca9df5abd8eed
Matteo Piraccini <matteo.piraccini@outlook.it>
2023-02-24 23:16:14 +0100

    24/02/2023 23:16

--- code/simulation.py
+++ code/simulation.py
@@ -7,6 +7,8 @@ Data = namedtuple( 'Data',
 	'sources',
 	'count_type'])
 
+##### Qui ci vuole una lista con tutti gli array con tutti i dati
+
 def init_simulation(): # che parametri ci vanno?
 
 	"""
@@ -19,15 +21,15 @@ def init_simulation(): # che parametri ci vanno?
 	
    	-----------------------------
 
-    	Returns
+    	Returns a tupla with a 3D array with data about the 0-generation, the matrix of reputation and an array that store the amount of every types of individual
 	
     	"""
 
 	#creation of the 0-generation
 
-	population=np.zeros(100, np.byte)
+	population=np.zeros((1,2,100), np.byte) # 1°-dim: generation, 2°-dim: strategy/sources, 3°-dim: individual
 	
-	population[0] = np.array(generate_random_int(-5, +7, (1,100), np.short)) #7 is excluse from the random number
+	population[0][0] = np.array(random_int(-5, +7, (1,100), np.short)) #7 is excluse from the random number
 
 	#setup of a separate array for the source of every individual, in order to save memory
 	
@@ -35,23 +37,19 @@ def init_simulation(): # che parametri ci vanno?
 
 	#setup of the matrix of reputation
 
-	reputation = np.zeros((100,100), np.byte)
+	image_matrix = np.zeros((100,100), np.byte)
 
 	#setup the matrix that will count the amount of every type related to generation
 
-	pop_count_type = np.array(count_population(population[0]))
+	pop_count_type = np.array(count_population(population[0]))	
 
-	O_data=Data(population,reputation, pop_count_type)
-
-	return O_data
-
-
-############################################# controllare se tupla permette di memorizzare diversi tipi di array
+	return Data(population, image_matrix, pop_count_type)
 		
 
-def generate_random_int(low_value, high_value, size, dtype):# tengo la generazione randomica separate per il testing
+def random_int(low_value, high_value, size, dtype):# tengo la generazione randomica separate per il testing
 
 	return np.random.randint(low_value, high_value, size, dtype)
+
 
 def count_population(array_pop):
 
@@ -61,8 +59,74 @@ def count_population(array_pop):
 
 		count_row[x]=len(array_pop[array_pop==x])
 
-	return count_row
+	return count_row ##### [0 1 2 3 4 5 6 -5 -4 -3 -2 -1]
 
+
+def interaction(donator, recipient, onlookers, strategy_array, sources_array, image_matrix):
+
+	"""
+
+    	A pair of individuals, donator-reipient, interact and donator decide whether cooperate
+
+    	Parameters:
+
+    	-----------------------------
+	
+	donator: the index that rappresent an individual from the population that could cooperate
+
+	recipient: the index that rappresent an individual from the population that maybe receive
+
+	onlookers: an array 1x10 of indexes of indivuals that observe the interaction
+
+	strategy_array: an array 1x100 with the strategies for each individuals
+
+	sources_array: an array 1x100 with the sources for each individuals
+
+	image_matrix: a matrix 100x100 with the image of each individuals (row) about other individuals (column)
+
+   	-----------------------------
+
+    	###Returns a tupla with the updated population_array and the updated reputation_matrix
+	
+    	"""
+
+	bonus=-1
+
+	if strategy_array[donator]>=image_matrix[donator][recipient]:
+		
+		bonus=1
+
+		sources_array[donator]-=0.1
+
+		sources_array[recipient]+=1
+
+		
+	image_matrix[recipient][donator]+=bonus
+
+
+	for x in range(len(onlookers)):
+
+		image_matrix[onlookers[x]][donator]+=1
+
+	###return (sources_array, image_matrix) (vale tenerlo con variabile locale?)
+
+def life():
+
+	"""
+
+    	The evolution of the life of the members of a generation
+
+    	Parameters:
+
+    	-----------------------------
+	
+	donator: 
+
+   	-----------------------------
+
+    	Returns
+	
+    	"""
 	
 		
 	
--- code/testing.py
+++ code/testing.py
@@ -5,16 +5,67 @@ import simulation as sm
 def testing_init():
 	# just a visual check
 	zero=np.zeros((2,100))
-	a=sm.init_simulation()[0]
-	print(a)
-	assert a.shape==zero.shape
-	assert a==zero
+	arrays_tupla=sm.init_simulation()
+	print(arrays_tupla)
+	assert arrays_tupla[0].shape==(1,2,100)
+	assert arrays_tupla[1].shape==(100,100)
+	assert arrays_tupla[2].shape==(12,)
+##### controllare la dimensionalità
 
 def testing_count(min,max):
 	# check if the counter work
+	
+	assert isinstance(min,int)
+
+	assert isinstance(max,int)
+
+	assert max>min
+
 	a=np.array(np.random.randint(min, max, 100, np.byte))
+		
 	count_row=sm.count_population(a)
+
 	print(count_row)
+		
 	print(a)
-	if(min==-5 & max==7):
-		assert np.sum(count_row)==100
+	
+
+def testing_interaction():
+
+	np.random.seed(1)
+
+	fake_indexes=np.random.randint(0,100, 12)
+
+
+	for x in range(len(fake_indexes)):
+		for y in range(len(fake_indexes)):
+
+			if x==y:
+				continue
+
+			assert fake_indexes[x]!=fake_indexes[y]
+
+	strategy=np.random.randint(-5,7,100)
+
+	sources=np.array(np.random.randint(-2,4,100), float)
+
+	image=np.zeros((100,100), np.byte)
+
+	print(fake_indexes)
+
+	print( sources)
+	
+	sm.interaction( fake_indexes[0], fake_indexes[1], fake_indexes[-10:], strategy, sources, image)
+
+	#check if reputation change
+
+	assert np.sum(image[image!=0]) == 11
+
+	#interactions work
+	
+
+
+
+
+
+
